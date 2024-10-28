a618c8a3503cfaa2524fb3c84eae2b043e05f606
suvendu1 <suvendumandal742@gmail.com>
2024-10-28 12:04:47 +0530

    check1

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -4,15 +4,15 @@ export default class NewsItem extends Component {
   render() {
     return (
       <div>
-        <div class="card" >
+        <div className="card" >
           
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">
               Some quick example text to build on the card title and make up the
               bulk of the card's content.
             </p>
-            <a href="/" class="btn btn-primary">
+            <a href="/" className="btn btn-primary">
               Go somewhere
             </a>
           </div>
