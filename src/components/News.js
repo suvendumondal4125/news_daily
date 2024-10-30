2b1c1de9079df2cfd2b08f29b38bfc859e8dc512
suvendu1 <suvendumandal742@gmail.com>
2024-10-30 17:34:40 +0530

    API

--- src/components/News.js
+++ src/components/News.js
@@ -2,49 +2,111 @@ import React, { Component } from "react";
 import NewsItem from "./NewsItem";
 
 export default class News extends Component {
+  articles = [
+    {
+      source: {
+        id: "espn-cric-info",
+        name: "ESPN Cric Info",
+      },
+      author: null,
+      title:
+        "PCB hands Umar Akmal three-year ban from all cricket | ESPNcricinfo.com",
+      description:
+        "Penalty after the batsman pleaded guilty to not reporting corrupt approaches | ESPNcricinfo.com",
+      url: "http://www.espncricinfo.com/story/_/id/29103103/pcb-hands-umar-akmal-three-year-ban-all-cricket",
+      urlToImage:
+        "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg",
+      publishedAt: "2020-04-27T11:41:47Z",
+      content:
+        "Umar Akmal's troubled cricket career has hit its biggest roadblock yet, with the PCB handing him a ban from all representative cricket for three years after he pleaded guilty of failing to report det… [+1506 chars]",
+    },
+    {
+      source: {
+        id: "espn-cric-info",
+        name: "ESPN Cric Info",
+      },
+      author: null,
+      title:
+        "What we learned from watching the 1992 World Cup final in full again | ESPNcricinfo.com",
+      description:
+        "Wides, lbw calls, swing - plenty of things were different in white-ball cricket back then | ESPNcricinfo.com",
+      url: "http://www.espncricinfo.com/story/_/id/28970907/learned-watching-1992-world-cup-final-full-again",
+      urlToImage:
+        "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg",
+      publishedAt: "2020-03-30T15:26:05Z",
+      content:
+        "Last week, we at ESPNcricinfo did something we have been thinking of doing for eight years now: pretend-live ball-by-ball commentary for a classic cricket match. We knew the result, yes, but we tried… [+6823 chars]",
+    },
+    {
+      source: {
+        id: "the-times-of-india",
+        name: "The Times of India",
+      },
+      author: "Times Of India",
+      title:
+        "PBKS vs MI Live Score, IPL 2021: Mumbai Indians seek consistency; Punjab Kings eye return to winning ways",
+      description:
+        "IPL Live Score: Mumbai Indians seek consistency; Punjab Kings eye return to winning ways. Stay with TOI to get IPL live score, playing 11, scorecard, highlights and ball by ball score updates of the 17th IPL match between Punjab Kings and Mumbai Indians.",
+      url: "http://timesofindia.indiatimes.com/sports/cricket/ipl/live-blog/punjab-kings-vs-mumbai-indians-pbks-vs-mi-live-score-ipl-2021-17th-match-chennai/liveblog/82214950.cms",
+      urlToImage:
+        "https://static.toiimg.com/thumb/msid-82214950,width-1070,height-580,imgsize-157009,resizemode-75,overlay-toi_sw,pt-32,y_pad-40/photo.jpg",
+      publishedAt: "2021-04-23T05:44:49Z",
+      content:
+        "Ravi Bishnoi return on the cards?\r\nDeepak Hooda showed what he is capable of, but greater consistency would be needed from him if the team has to prosper. They bet on Australian pace imports Jhye Ric… [+3131 chars]",
+    },
+    {
+      source: {
+        id: "ars-technica",
+        name: "Ars Technica",
+      },
+      author: "Jon Brodkin",
+      title:
+        "T-Mobile pays $16 million fine for three years’ worth of data breaches",
+      description:
+        "Breaches in three consecutive years lead to $16M fine and new security protocols.",
+      url: "https://arstechnica.com/tech-policy/2024/10/t-mobile-pays-16-million-fine-for-three-years-worth-of-data-breaches/",
+      urlToImage:
+        "https://cdn.arstechnica.net/wp-content/uploads/2024/05/t-mobile-760x380.jpg",
+      publishedAt: "2024-10-01T18:39:24+00:00",
+      content:
+        '45\r\nT-Mobile has agreed to pay a $15.75 million fine and improve its security in a settlement over a series of data breaches over three years that affected tens of millions of customers.\r\n"T-Mobile s… [+2919 chars]',
+    },
+  ];
+
+  constructor() {
+    super();
+    // console.log("This is news.");
+    this.state = {
+      articles: this.articles,
+      loading: false,
+    };
+  }
+  async componentDidMount(){
+    console.log("fetch");
+    let url = "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=70bc2cfd25f14eba866a7ec572c46d55";
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    console.log(parsedData);
+    this.setState({articles:parsedData.articles})
+  }
+
   render() {
     return (
       <div className="container my-3">
         <h2>News-Daily Top Headlince</h2>
         <div className="row">
-          <div className="col-md-3">
-            <NewsItem
-              title="This is title"
-              description="This is description"
-              imgUrl=""
-            />
-          </div>
-          <div className="col-md-3" >
-            <NewsItem
-              title="This is title"
-              imgUrl=""
-              description={
-                <>
-                  This is <strong>our</strong> description
-                </>
-              }
-            />
-          </div>
-          <div className="col-md-3">
-            <NewsItem
-              title="This is title"
-              description={
-                <>
-                  This is <strong>our</strong> description
-                </>
-              }
-            />
-          </div>
-          <div className="col-md-3">
-            <NewsItem
-              title="This is title"
-              description={
-                <>
-                  This is <strong>our</strong> description
-                </>
-              }
-            />
-          </div>
+          {this.state.articles.map((element) => {
+            return (
+              <div className="col-md-3" key={element.url}>
+                <NewsItem
+                  title={element.title?element.title.slice(0, 45):""}
+                  description={element.description?element.description.slice(0, 88):""}
+                  imgUrl={element.urlToImage}
+                  newsUrl={element.url}
+                />
+              </div>
+            );
+          })}
         </div>
       </div>
     );
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -6,17 +6,17 @@ export default class NewsItem extends Component {
     console.log("hello")
   }
   render() {
-    let {title, description, imgUrl} = this.props;
+    let {title, description, imgUrl, newsUrl} = this.props;
     return (
-      <div className="container my-3">
-        <div className="card" style={{width: "18rem"}} >
+      <div className="my-3">
+        <div className="card" >
           <img src={imgUrl} className="card-img-top" alt="..." />
           <div className="card-body">
-            <h5 className="card-title">{title}</h5>
-            <p className="card-text">
-             {description}
+            <h5 className="card-title">{title}...</h5>
+            <p className="card-text ">
+             {description}...
             </p>
-            <a href="/" className="btn btn-sm btn-primary">
+            <a href={newsUrl} target="{_blank}" className="btn btn-sm btn-primary">
               Read More
             </a>
           </div>
